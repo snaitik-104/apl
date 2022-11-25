1d87c3a7c300c91f3bf83e83f3cf884644e4ca62
Naitik Sharma <snaitik104@gmail.com>
2022-11-25 22:45:03 +0530

    Update PointsTable.jsx

--- src/Components/PointsTable.jsx
+++ src/Components/PointsTable.jsx
@@ -12,12 +12,9 @@ function PointsTable() {
 					<th title="Team name" className="p-3 text-left">Team</th>
 					<th title="Wins" className="p-3">W</th>
 					<th title="Losses" className="p-3">L</th>
-					<th title="Win percentage" className="p-3">Win%</th>
-					<th title="Games behind" className="p-3">GB</th>
-					<th title="Home games" className="p-3">Home</th>
-					<th title="Away games" className="p-3">Away</th>
-					<th title="Last 10 games" className="p-3">L10</th>
-					<th title="Current streak" className="p-3">Streak</th>
+					<th title="Win percentage" className="p-3">POINTS</th>
+					<th title="Games behind" className="p-3">NET RR</th>
+					
 				</tr>
 			</thead>
 			<tbody>
@@ -40,18 +37,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>0</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>17-5</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>14-12</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>8-2</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>W2</span>
-					</td>
+				
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -72,18 +58,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>0.5</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>17-6</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>13-11</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>4-6</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>W2</span>
-					</td>
+				
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -104,18 +79,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>1.5</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>15-9</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>15-10</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>7-3</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>W3</span>
-					</td>
+					
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -136,18 +100,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>2</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>12-13</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>17-6</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>4-6</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>L3</span>
-					</td>
+					
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -168,18 +121,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>2</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>17-8</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>13-12</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>5-5</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>L1</span>
-					</td>
+				
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -200,18 +142,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>2.5</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>11-10</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>17-9</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>7-3</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>W2</span>
-					</td>
+				
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -232,18 +163,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>4.5</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>13-7</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>14-15</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>7-3</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>W1</span>
-					</td>
+					
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -264,18 +184,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>6.5</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>16-10</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>9-14</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>7-3</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>W2</span>
-					</td>
+					
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -296,18 +205,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>7</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>13-12</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>10-11</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>4-6</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>L1</span>
-					</td>
+				
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -328,18 +226,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>8</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>13-12</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>10-12</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>4-6</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>L4</span>
-					</td>
+				
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -360,18 +247,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>8.5</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>12-14</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>11-12</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>4-6</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>L2</span>
-					</td>
+					
 				</tr>
 				<tr className="text-right border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-800">
 					<td className="px-3 py-2 text-left">
@@ -392,18 +268,7 @@ function PointsTable() {
 					<td className="px-3 py-2 text-right">
 						<span>8.5</span>
 					</td>
-					<td className="px-3 py-2">
-						<span>12-11</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>10-14</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>5-5</span>
-					</td>
-					<td className="px-3 py-2">
-						<span>W5</span>
-					</td>
+				
 				</tr>
 			</tbody>
 		</table>
